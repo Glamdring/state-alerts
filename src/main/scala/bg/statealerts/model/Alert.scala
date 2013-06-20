091ca6d6acf883253f8d01f72c6af2576b091c7e
bozho <glamd@abv.bg>
2013-06-21 00:13:04 +0300

    model

--- src/main/scala/bg/statealerts/model/Alert.scala
+++ src/main/scala/bg/statealerts/model/Alert.scala
@@ -1,8 +1,22 @@
 package bg.statealerts.model
 
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Column
+import javax.persistence.ElementCollection
+
+@Entity
 class Alert {
 
-  var email: String;
-  var name: String;
+  @Id @GeneratedValue(strategy=GenerationType.AUTO)
+  var id: Int = _
+  @Column
+  var email: String = _
+  @Column
+  var name: String = _
+  @ElementCollection
   var keywords: List[String] = List();
+  
 }
