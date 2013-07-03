9b9c8a4537a520ebee14db7814c2e41223e8e88b
Glamdring <glamd@abv.bg>
2013-07-03 11:36:10 +0300

    spring context added; cachegenerator and modelampper; build succeeding

---
+++ src/main/scala/bg/statealerts/util/CacheKeyGenerator.scala
@@ -0,0 +1,49 @@
+package bg.statealerts.util
+
+import java.lang.reflect.Method
+import org.springframework.cache.interceptor.KeyGenerator
+import org.apache.log4j.spi.LoggerFactory
+import org.slf4j.Logger
+import org.apache.commons.lang3.ClassUtils
+import org.springframework.expression.spel.support.ReflectivePropertyAccessor.CacheKey
+import scala.Enumeration
+import scala.Enumeration
+
+/**
+ * Copied from DefaultKeyGenerator, but using the class method names in the key as well
+ *
+ * @author bozho
+ *
+ */
+class CacheKeyGenerator extends KeyGenerator {
+	//val logger: Logger = LoggerFactory.getLogger(classOf[DSPCacheKeyGenerator]);
+
+	val NoParamKey: Int = 0;
+	val NullParamKey: Int = 53;
+
+	def generate(target: Any, method: Method, params: Object*): Object = {
+		var key: StringBuilder = new StringBuilder();
+		key.append(target.getClass().getSimpleName()).append(".").append(method.getName()).append(":");
+
+		if (params.length == 0) {
+			return key.append(NoParamKey).toString();
+		}
+
+		for (param <- params) {
+			if (param == null) {
+				key.append(NullParamKey);
+			} else if (ClassUtils.isPrimitiveOrWrapper(param.getClass()) || param.isInstanceOf[String]) {
+				key.append(param);
+			} else if (param.getClass().isEnum()) {
+				key.append(param.toString());
+			} else {
+				//logger.warn("Using an object as a cache key may lead to unexpected results. "
+				//		+ "Either use @Cacheable(key=..) or implement CacheKey. Method is " + target.getClass() + "#"
+				//		+ method.getName());
+				key.append(param.hashCode());
+			}
+		}
+
+		return key.toString();
+	}
+}
---
+++ src/main/scala/bg/statealerts/util/ModelMapperConverter.scala
@@ -0,0 +1,28 @@
+package bg.statealerts.util
+
+package com.ontotext.dsp.util
+
+import java.util.Collections
+import java.util.Set
+import org.modelmapper.ModelMapper
+import org.springframework.beans.factory.InitializingBean
+import org.springframework.core.convert.TypeDescriptor
+import org.springframework.core.convert.converter.GenericConverter
+import org.springframework.core.convert.converter.GenericConverter.ConvertiblePair
+
+class ModelMapperConverter extends GenericConverter with InitializingBean {
+
+	var mapper: ModelMapper = new ModelMapper();
+	
+	def afterPropertiesSet() = {
+		// use mapper.getConfiguration() to setup ModelMapper
+	}
+	
+	def getConvertibleTypes(): Set[ConvertiblePair] = {
+		return Collections.singleton(new ConvertiblePair(classOf[Any], classOf[Any]));
+	}
+
+	def convert(source: Any, sourceType: TypeDescriptor, targetType: TypeDescriptor): Object = {
+		return mapper.map(source, targetType.getObjectType())
+	}
+}
