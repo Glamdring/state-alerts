a63009ea7672da5ef951cb4c932dfc5948849217
Glamdring <glamd@abv.bg>
2013-07-03 10:48:49 +0300

    extractor creation; dao

---
+++ src/main/scala/bg/statealerts/dao/BaseDao.scala
@@ -0,0 +1,28 @@
+package bg.statealerts.dao
+
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+import org.springframework.stereotype.Repository
+import javax.persistence.TypedQuery
+import org.joda.time.DateTime
+
+@Repository
+class BaseDao {
+
+  @PersistenceContext
+  var entityManager: EntityManager = _
+  
+  def save[T](entity: T): T = {
+    entityManager.merge(entity);
+  }
+  
+  def get[T](clazz: Class[T], id:Any): T = {
+    entityManager.find(clazz, id);
+  }
+ 
+  def getLastImportDate(): DateTime = {
+    var query: TypedQuery[DateTime] = entityManager.createQuery("SELECT importTime FROM Import ORDER BY importTime DESC", classOf[DateTime])
+    query.setMaxResults(1)
+    query.getSingleResult()
+  }
+}
--- src/main/scala/bg/statealerts/scheduled/InformationExtractorJob.scala
+++ src/main/scala/bg/statealerts/scheduled/InformationExtractorJob.scala
@@ -1,14 +1,16 @@
 package bg.statealerts.scheduled
 
 import java.io.File
-
 import org.codehaus.jackson.map.ObjectMapper
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-
 import bg.statealerts.services.InformationExtractor
 import javax.annotation.PostConstruct
+import bg.statealerts.services.extractors.XPathExtractor
+import javax.inject.Inject
+import bg.statealerts.dao.BaseDao
+import bg.statealerts.model.Document
 
 @Component
 class InformationExtractorJob {
@@ -17,21 +19,33 @@ class InformationExtractorJob {
   val mapper: ObjectMapper = new ObjectMapper()
   
   @Value("${config.location}")
-  var configLocation:String = _
+  var configLocation: String = _
+  
+  @Inject
+  var dao: BaseDao = _
   
   @PostConstruct
   def init() {
     var file:File = new File(configLocation + "/extractors.xml")
-    var config = mapper.readValue(file, classOf[ExtractorConfiguration]);
-    for (descriptor <- config.extractors) {
-      var extractor = Class.forName("bg.statealerts.services.extractors." + descriptor.extractorType + "Extractor").newInstance();
-      extractor :: extractors;
+    var config = mapper.readValue(file, classOf[ExtractorConfiguration])
+    for (descriptor <- config.extractors) yield {
+      var extractor: InformationExtractor = null
+      descriptor.extractorType match {
+        case "XPath" => extractor = getXPathExtractor(descriptor)
+      }
+      extractor :: extractors
     }
   }
   
   @Scheduled(fixedRate=100000)
   def run() {
+    var lastImportTime = dao.getLastImportDate();
     for (extractor <- extractors) {
+      var document: List[Document] = extractor.extract(lastImportTime);
     }
+  }
+  
+  private def getXPathExtractor(descriptor: ExtractorDescriptor): InformationExtractor = {
+    new XPathExtractor(descriptor.contentPath, descriptor.titlePath, descriptor.datePath, descriptor.dateFormat);
   }
 }
