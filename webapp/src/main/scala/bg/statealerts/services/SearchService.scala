ebf7d966da2a816323afec7d019f86c41b85b027
bozho <glamd@abv.bg>
2013-09-02 08:52:33 +0300

    build fix

--- webapp/src/main/scala/bg/statealerts/services/SearchService.scala
+++ webapp/src/main/scala/bg/statealerts/services/SearchService.scala
@@ -1,17 +1,16 @@
 package bg.statealerts.services
 
 import java.io.File
-
 import org.apache.lucene.analysis.Analyzer
-import org.apache.lucene.index.IndexReader
 import org.apache.lucene.index.Term
 import org.apache.lucene.queryparser.flexible.standard.QueryParserUtil
 import org.apache.lucene.search.BooleanClause
 import org.apache.lucene.search.BooleanQuery
-import org.apache.lucene.search.IndexSearcher
 import org.apache.lucene.search.NumericRangeQuery
 import org.apache.lucene.search.Query
 import org.apache.lucene.search.ScoreDoc
+import org.apache.lucene.search.SearcherFactory
+import org.apache.lucene.search.SearcherManager
 import org.apache.lucene.search.Sort
 import org.apache.lucene.search.SortField
 import org.apache.lucene.search.TermQuery
@@ -19,19 +18,19 @@ import org.apache.lucene.search.TopDocs
 import org.apache.lucene.store.FSDirectory
 import org.apache.lucene.util.Version
 import org.joda.time.DateTime
+import org.joda.time.DateTimeConstants
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.DependsOn
 import org.springframework.scheduling.annotation.Scheduled
-import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-
 import bg.statealerts.dao.DocumentDao
 import bg.statealerts.model.Document
 import javax.annotation.PostConstruct
 import javax.annotation.PreDestroy
 import javax.inject.Inject
-import javax.persistence.Entity
+import org.apache.lucene.index.ReaderManager
+import org.apache.lucene.search.IndexSearcher
 
 @Service
 @DependsOn(Array("indexer")) // indexer initializes index
@@ -47,17 +46,17 @@ class SearchService {
   @Value("${lucene.analyzer.class}")
   var analyzerClass: String = _
   
-  var reader: IndexReader = _
+  var readerManager: ReaderManager = _
   
   @PostConstruct
   def init() = {
-    reader = getNewReader()
+    readerManager = new ReaderManager(FSDirectory.open(new File(indexPath)))
     analyzer = Class.forName(analyzerClass).getConstructor(classOf[Version]).newInstance(Version.LUCENE_43).asInstanceOf[Analyzer]
   }
 
   @PreDestroy
   def destroy() = {
-    reader.close()
+    readerManager.close()
   }
   
   @Transactional(readOnly=true)
@@ -84,30 +83,31 @@ class SearchService {
   }
   
   private def getDocuments(query: Query, limit: Int): List[Document] = {
+    val reader = readerManager.acquire()
     
-	val searcher = new IndexSearcher(reader)
-    val sort = new Sort(new SortField("publishTimestamp", SortField.Type.LONG, true))
-    val result: TopDocs = searcher.search(query, null, limit, sort)
-
-    var documents = List[Document]()
-
-    val topDocs: Array[ScoreDoc] = result.scoreDocs
-    
-    for (topDoc <- topDocs) {
-      val luceneDoc = searcher.doc(topDoc.doc)
-      val doc = documentDao.get(classOf[Document], luceneDoc.get("id").toInt)
-      documents ::= doc
+    try {
+    	val searcher = new IndexSearcher(reader)
+	    val sort = new Sort(new SortField("publishTimestamp", SortField.Type.LONG, true))
+	    val result: TopDocs = searcher.search(query, null, limit, sort)
+	
+	    var documents = List[Document]()
+	
+	    val topDocs: Array[ScoreDoc] = result.scoreDocs
+	    
+	    for (topDoc <- topDocs) {
+	      val luceneDoc = searcher.doc(topDoc.doc)
+	      val doc = documentDao.get(classOf[Document], luceneDoc.get("id").toInt)
+	      documents ::= doc
+	    }
+	    documents
+    } finally {
+      readerManager.release(reader)
     }
-    documents
   }
   
-  @Scheduled(fixedRate = 600000) // 10 minutes, reopen the reader to make new documents searchable
-  def refreshSearchers() = {
-    reader.close()
-    reader = getNewReader()
-  }
-  
-  private def getNewReader(): IndexReader = {
-    return IndexReader.open(FSDirectory.open(new File(indexPath)))
+  @Scheduled(fixedRate = 60000) // 10 minutes
+  def refreshReaderManager(): Unit = {
+    println("ALABALA")
+    readerManager.maybeRefresh()
   }
 }
