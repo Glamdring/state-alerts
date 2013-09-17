71f32326d11f8b4832910d955b93cf635dc25ce4
Vladimir Tsanev <tsachev@gmail.com>
2013-09-17 07:21:03 +0300

    authentication cookies are now set on the default domain. we may add configuration to override it if we need to.

--- webapp/src/main/scala/bg/statealerts/controllers/SocialSignInAdapter.scala
+++ webapp/src/main/scala/bg/statealerts/controllers/SocialSignInAdapter.scala
@@ -3,11 +3,13 @@ package bg.statealerts.controllers;
 import javax.inject.Inject
 import javax.servlet.http.Cookie
 import javax.servlet.http.HttpServletResponse
+import javax.servlet.http.HttpServletRequest
 import org.joda.time.DateTimeConstants
 import org.springframework.social.connect.Connection
 import org.springframework.social.connect.web.SignInAdapter
 import org.springframework.stereotype.Component
 import org.springframework.web.context.request.NativeWebRequest
+import org.springframework.web.util.WebUtils
 import bg.statealerts.services.UserService
 import bg.statealerts.model.User
 
@@ -22,29 +24,53 @@ class SocialSignInAdapter extends SignInAdapter {
 
     override def signIn(userId: String, connection: Connection[_], request: NativeWebRequest ): String = {
         val user = userService.getUser(userId.toLong);
-        signIn(user, request.getNativeResponse().asInstanceOf[HttpServletResponse], true);
+        signIn(user, request.getNativeResponse(classOf[HttpServletRequest]), request.getNativeResponse(classOf[HttpServletResponse]), true);
         return "/";
     }
 
-    def signIn(user: User , response: HttpServletResponse, resetTokens: Boolean) = {
+    def signIn(user: User, request: HttpServletRequest, response: HttpServletResponse, resetTokens: Boolean) = {
         context.user = user;
         if (resetTokens) {
             userService.fillUserWithNewTokens(user, null);
         }
-        addPermanentCookies(user, response);
+        addPermanentCookies(user, request, response)
     }
 
-    def addPermanentCookies(user: User, response: HttpServletResponse) = {
-        val authTokenCookie = new Cookie(Constants.AuthTokenCookieName, user.loginToken);
-        authTokenCookie.setMaxAge(Constants.CookieAge);
-        authTokenCookie.setPath("/");
-        authTokenCookie.setDomain(".statealerts.com");
-        response.addCookie(authTokenCookie);
+    def signOut(request: HttpServletRequest, response: HttpServletResponse) = {
+        removePermanentCookies(request, response)
+    }
 
-        val seriesCookie = new Cookie(Constants.AuthTokenSeriesCookieName, user.loginSeries);
-        seriesCookie.setMaxAge(Constants.CookieAge);
-        seriesCookie.setPath("/");
-        seriesCookie.setDomain(".statealerts.com");
-        response.addCookie(seriesCookie);
+    def addPermanentCookies(user: User, request: HttpServletRequest, response: HttpServletResponse) = {
+        val authTokenCookie = new Cookie(Constants.AuthTokenCookieName, user.loginToken)
+        authTokenCookie.setMaxAge(Constants.CookieAge)
+        authTokenCookie.setPath(cookiePath(request))
+        response.addCookie(authTokenCookie)
+
+        val seriesCookie = new Cookie(Constants.AuthTokenSeriesCookieName, user.loginSeries)
+        seriesCookie.setMaxAge(Constants.CookieAge)
+        seriesCookie.setPath(cookiePath(request))
+        response.addCookie(seriesCookie)
+    }
+
+    def removePermanentCookies(request: HttpServletRequest, response: HttpServletResponse)
+    {
+        val cookie = WebUtils.getCookie(request, Constants.AuthTokenCookieName)
+        if (cookie != null) {
+            cookie.setMaxAge(0)
+            cookie.setPath(cookiePath(request))
+            response.addCookie(cookie)
+        }
+
+        val seriesCookie = WebUtils.getCookie(request, Constants.AuthTokenSeriesCookieName)
+        if (seriesCookie != null) {
+            seriesCookie.setMaxAge(0)
+            seriesCookie.setPath(cookiePath(request))
+            response.addCookie(seriesCookie)
+        }
+    }
+
+    def cookiePath(request: HttpServletRequest) = {
+        // we currently need this cookie only for logout
+        request.getContextPath + "/logout"
     }
 }
